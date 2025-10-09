0fd6186b58790982e3f068de9db5e91850b0169b
Loveleen C <loveleenchaudhari@gmail.com>
2025-10-09 15:41:50 +0530

    add maven.properties, add component annotation to ticketmapper

--- src/main/java/com/bookit/application/persistence/jdbcDao/mappers/TicketMapper.java
+++ src/main/java/com/bookit/application/persistence/jdbcDao/mappers/TicketMapper.java
@@ -2,11 +2,13 @@ package com.bookit.application.persistence.jdbcDao.mappers;
 
 import com.bookit.application.entity.*;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.UUID;
 
+@Component
 public class TicketMapper implements RowMapper<Ticket> {
     private SeatMapper seatMapper;
 
