74e1bc7bb1b42486185f4ce922a80090dd7b8ef7
Loveleen C <loveleenchaudhari@gmail.com>
2026-01-09 19:28:25 +0530

    fix user tests

--- src/test/java/com/bookit/application/services/UserTest.java
+++ src/test/java/com/bookit/application/services/UserTest.java
@@ -1,19 +1,18 @@
 package com.bookit.application.services;
 
-import com.bookit.application.persistence.ICartDao;
 import com.bookit.application.security.CustomUserDetailsService;
+import com.bookit.application.security.email.EmailService;
 import com.bookit.application.security.entity.User;
-import com.bookit.application.services.email.EmailService;
+import com.bookit.application.security.entity.types.Role;
+import com.bookit.application.security.user.UserAccountCreationHelper;
 import com.bookit.application.security.user.UserService;
+import com.bookit.application.security.user.booking.BookingClient;
 import com.bookit.application.security.user.token.TokenService;
-import com.bookit.application.types.Role;
-import com.bookit.application.utils.UserAccountCreationHelper;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.core.io.UrlResource;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import java.util.List;
@@ -21,168 +20,153 @@ import java.util.List;
 import static org.mockito.Mockito.*;
 
 public class UserTest {
-  private static UserService userService;
-  private static PasswordEncoder passwordEncoder;
-  private static CustomUserDetailsService customUserDetailsService;
-  private static TokenService tokenService;
-  private static EmailService emailService;
-  private static ICartDao cartDao;
-  private static UserAccountCreationHelper userAccountCreationHelper;
-  private User user;
-  //user creation fails
-  //username - length is less than 8 and more than 15
-  //username - has characters other than uppercase/lowercase and digits
+    private static UserService userService;
+    private static CustomUserDetailsService customUserDetailsService;
+    private static TokenService tokenService;
+    private static EmailService emailService;
+    private static UserAccountCreationHelper userAccountCreationHelper;
+    private static BookingClient bookingClient;
+    private User user;
 
 
-  //password - length is less than 8 and more than 20
+    @BeforeAll
+    public static void beforeAll() {
+        customUserDetailsService = mock(CustomUserDetailsService.class);
+        tokenService = mock(TokenService.class);
+        emailService = mock(EmailService.class);
+        userAccountCreationHelper = mock(UserAccountCreationHelper.class);
+        bookingClient = mock(BookingClient.class);
+        userService = new UserService(customUserDetailsService,
+                tokenService, emailService, userAccountCreationHelper, bookingClient);
+    }
 
-  //password -  doesnt contain digits
-  //password - doesnt contain uppercase or lowercase characters
-  //password - doesnt special characters
+    @BeforeEach
+    public void before() {
+        this.user = new User("Bob",
+                "Marley", "bobbyTable1", "hyh9ssH8*", "marleybob@mail.com",
+                List.of(Role.REGULAR_USER));
+    }
 
-  //email - matches email regex
+    @Test
+    public void test_userCreationFailsWhenUsernameLengthLessThan8AndMoreThan15_OrHasSpecialCharacters() {
+        this.user.setUsername("abC21aa");
+        Assertions.assertThrows(IllegalArgumentException.class, () -> userService.createUser(this.user),
+                "User creation fails when username is less than 8 characters");
 
+        this.user.setUsername("userj7h5dG5jaqw");
+        Assertions.assertThrows(IllegalArgumentException.class, () -> userService.createUser(this.user));
 
-  //create a user successfully
+        this.user.setUsername("userj7hsi@");
+        Assertions.assertThrows(IllegalArgumentException.class, () -> userService.createUser(this.user));
+    }
 
-  @BeforeAll
-  public static void beforeAll() {
-    customUserDetailsService = mock(CustomUserDetailsService.class);
-    tokenService = mock(TokenService.class);
-    emailService = mock(EmailService.class);
-    userAccountCreationHelper = mock(UserAccountCreationHelper.class);
-    cartDao = mock(ICartDao.class);
-    userService = new UserService(customUserDetailsService,
-      tokenService, emailService, cartDao, userAccountCreationHelper);
-  }
+    @Test
+    public void test_userCreationFailsWhenPasswordDoesNotMatchRequiredCriteria() {
+        this.user.setPassword("*aA124s");
+        Assertions.assertThrows(IllegalArgumentException.class, () -> userService.createUser(this.user),
+                "User creation fails when password is less than 8 characters");
 
-  @BeforeEach
-  public void before() {
-    this.user = new User("Bob",
-      "Marley", "bobbyTable1", "hyh9ssH8*", "marleybob@mail.com",
-      List.of(Role.REGULAR_USER));
-  }
+        this.user.setPassword("*aA124aqtd8d&nGTkaq1");
+        Assertions.assertThrows(IllegalArgumentException.class, () -> userService.createUser(this.user),
+                "User creation fails when password is more than 20 characters");
 
-  @Test
-  public void test_userCreationFailsWhenUsernameLengthLessThan8AndMoreThan15_OrHasSpecialCharacters() {
-    this.user.setUsername("abC21aa");
-    Assertions.assertThrows(IllegalArgumentException.class, () -> userService.createUser(this.user),
-      "User creation fails when username is less than 8 characters");
+        this.user.setPassword("HY&*gtauho");
+        Assertions.assertThrows(IllegalArgumentException.class, () -> userService.createUser(this.user),
+                "User creation fails when password does not contain a digit");
 
-    this.user.setUsername("userj7h5dG5jaqw");
-    Assertions.assertThrows(IllegalArgumentException.class, () -> userService.createUser(this.user));
+        this.user.setPassword("HY&*3QWEQ3");
+        Assertions.assertThrows(IllegalArgumentException.class, () -> userService.createUser(this.user),
+                "User creation fails when password does not contain an lowercase character");
 
-    this.user.setUsername("userj7hsi@");
-    Assertions.assertThrows(IllegalArgumentException.class, () -> userService.createUser(this.user));
-  }
+        this.user.setPassword("htft**8@1qa");
+        Assertions.assertThrows(IllegalArgumentException.class, () -> userService.createUser(this.user),
+                "User creation fails when password does not contain an uppercase character");
 
-  @Test
-  public void test_userCreationFailsWhenPasswordDoesNotMatchRequiredCriteria() {
-    this.user.setPassword("*aA124s");
-    Assertions.assertThrows(IllegalArgumentException.class, () -> userService.createUser(this.user),
-      "User creation fails when password is less than 8 characters");
+        this.user.setPassword("Hyujgfrhj9");
+        Assertions.assertThrows(IllegalArgumentException.class, () -> userService.createUser(this.user),
+                "User creation fails when password does not contain a special character");
+    }
 
-    this.user.setPassword("*aA124aqtd8d&nGTkaq1");
-    Assertions.assertThrows(IllegalArgumentException.class, () -> userService.createUser(this.user),
-      "User creation fails when password is more than 20 characters");
-
-    this.user.setPassword("HY&*gtauho");
-    Assertions.assertThrows(IllegalArgumentException.class, () -> userService.createUser(this.user),
-      "User creation fails when password does not contain a digit");
-
-    this.user.setPassword("HY&*3QWEQ3");
-    Assertions.assertThrows(IllegalArgumentException.class, () -> userService.createUser(this.user),
-      "User creation fails when password does not contain an lowercase character");
-
-    this.user.setPassword("htft**8@1qa");
-    Assertions.assertThrows(IllegalArgumentException.class, () -> userService.createUser(this.user),
-      "User creation fails when password does not contain an uppercase character");
-
-    this.user.setPassword("Hyujgfrhj9");
-    Assertions.assertThrows(IllegalArgumentException.class, () -> userService.createUser(this.user),
-      "User creation fails when password does not contain a special character");
-  }
-
-  @Test
-  public void test_userCreationFailsWhenEmailDoesNotMatchRegex() {
-    this.user.setEmail("blablablah@nomail");
-    Assertions.assertThrows(IllegalArgumentException.class, () -> userService.createUser(this.user),
-      "User creation fails when email does not match the expected format");
+    @Test
+    public void test_userCreationFailsWhenEmailDoesNotMatchRegex() {
+        this.user.setEmail("blablablah@nomail");
+        Assertions.assertThrows(IllegalArgumentException.class, () -> userService.createUser(this.user),
+                "User creation fails when email does not match the expected format");
 
 
-  }
+    }
 
 
-  //if no role is specified
-  // if role is admin
-  // if role is theatreowner - account creation email is sent
-  // if role is regular user - account activation email is sent
-  // if role is regular user - cart is created
-  //
+    //if no role is specified
+    // if role is admin
+    // if role is theatreowner - account creation email is sent
+    // if role is regular user - account activation email is sent
+    // if role is regular user - cart is created
+    //
 
-  @Test
-  public void test_userCreationFailsWhenInvalidRolesAreProvided() {
-    this.user.setRoles(List.of());
-    Assertions.assertThrows(IllegalArgumentException.class, () -> userService.createUser(this.user),
-      "User creation fails when no roles are specified for the user");
+    @Test
+    public void test_userCreationFailsWhenInvalidRolesAreProvided() {
+        this.user.setRoles(List.of());
+        Assertions.assertThrows(IllegalArgumentException.class, () -> userService.createUser(this.user),
+                "User creation fails when no roles are specified for the user");
 
-    this.user.setRoles(List.of(Role.ADMIN));
-    Assertions.assertThrows(IllegalArgumentException.class, () -> userService.createUser(this.user),
-      "User creation fails when ADMIN role is specified for the user");
-  }
+        this.user.setRoles(List.of(Role.ADMIN));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> userService.createUser(this.user),
+                "User creation fails when ADMIN role is specified for the user");
+    }
 
-  @Test
-  public void test_verifyCreationOfValidUserAsTheatreOperator() {
-    this.user.setRoles(List.of(Role.THEATRE_OWNER));
-    User createdUser = new User(
-      this.user.getFirstName(),
-      this.user.getLastName(),
-      this.user.getUsername(),
-      this.user.getPassword(),
-      this.user.getEmail(),
-      this.user.getRoles()
-    );
-    createdUser.setId(1L);
-    when(customUserDetailsService.createUser(this.user)).thenReturn(createdUser);
-    when(userAccountCreationHelper.usernameCriteriaFulfilled(user.getUsername())).thenReturn(true);
-    when(userAccountCreationHelper.passwordCriteriaFulfilled(user.getPassword())).thenReturn(true);
-    when(userAccountCreationHelper.emailIsValid(user.getEmail())).thenReturn(true);
-    String randomMessage = "blah blah blah";
-    when(userAccountCreationHelper.createAccountActivationEmailMessage(user.getUsername())).thenReturn(randomMessage);
+    @Test
+    public void test_verifyCreationOfValidUserAsTheatreOperator() {
+        this.user.setRoles(List.of(Role.THEATRE_OWNER));
+        User createdUser = new User(
+                this.user.getFirstName(),
+                this.user.getLastName(),
+                this.user.getUsername(),
+                this.user.getPassword(),
+                this.user.getEmail(),
+                this.user.getRoles()
+        );
+        createdUser.setId(1L);
+        when(customUserDetailsService.createUser(this.user)).thenReturn(createdUser);
+        when(userAccountCreationHelper.usernameCriteriaFulfilled(user.getUsername())).thenReturn(true);
+        when(userAccountCreationHelper.passwordCriteriaFulfilled(user.getPassword())).thenReturn(true);
+        when(userAccountCreationHelper.emailIsValid(user.getEmail())).thenReturn(true);
+        String randomMessage = "blah blah blah";
+        when(userAccountCreationHelper.createAccountActivationEmailMessage(user.getUsername())).thenReturn(randomMessage);
 
-    userService.createUser(this.user);
-    verify(userAccountCreationHelper, times(1)).createAccountActivationEmailMessage(user.getUsername());
-    verify(emailService, times(1)).sendEmail(user.getEmail(),
-      UserAccountCreationHelper.accountActivationEmailSubject, randomMessage);
+        userService.createUser(this.user);
+        verify(userAccountCreationHelper, times(1)).createAccountActivationEmailMessage(user.getUsername());
+        verify(emailService, times(1)).sendEmail(user.getEmail(),
+                UserAccountCreationHelper.accountActivationEmailSubject, randomMessage);
 
-  }
+    }
 
 
-  @Test
-  public void test_verifyCreationOfValidUserAsCustomer() {
-    this.user.setRoles(List.of(Role.REGULAR_USER));
-    User createdUser = new User(
-      this.user.getFirstName(),
-      this.user.getLastName(),
-      this.user.getUsername(),
-      this.user.getPassword(),
-      this.user.getEmail(),
-      this.user.getRoles()
-    );
-    createdUser.setId(1L);
-    String randomMessage2 = "more blah blah blah welcoming crap";
-    ReflectionTestUtils.setField(userService, "clientUrl", "https://example.com/");
+    @Test
+    public void test_verifyCreationOfValidUserAsCustomer() {
+        this.user.setRoles(List.of(Role.REGULAR_USER));
+        User createdUser = new User(
+                this.user.getFirstName(),
+                this.user.getLastName(),
+                this.user.getUsername(),
+                this.user.getPassword(),
+                this.user.getEmail(),
+                this.user.getRoles()
+        );
+        createdUser.setId(1L);
+        String randomMessage2 = "more blah blah blah welcoming crap";
+        ReflectionTestUtils.setField(userService, "clientUrl", "https://example.com/");
 
-    when(customUserDetailsService.createUser(this.user)).thenReturn(createdUser);
-    when(userAccountCreationHelper.usernameCriteriaFulfilled(user.getUsername())).thenReturn(true);
-    when(userAccountCreationHelper.passwordCriteriaFulfilled(user.getPassword())).thenReturn(true);
-    when(userAccountCreationHelper.emailIsValid(user.getEmail())).thenReturn(true);
-    when(userAccountCreationHelper.
-      createAccountActivationEmailMessage(any(String.class), any(UrlResource.class)))
-      .thenReturn(randomMessage2);
-    userService.createUser(this.user);
-    verify(emailService, times(1)).sendEmail(user.getEmail(),
-      UserAccountCreationHelper.accountActivationEmailSubject, randomMessage2);
-    verify(cartDao, times(1)).createCart(createdUser.getId());
-  }
+        when(customUserDetailsService.createUser(this.user)).thenReturn(createdUser);
+        when(userAccountCreationHelper.usernameCriteriaFulfilled(user.getUsername())).thenReturn(true);
+        when(userAccountCreationHelper.passwordCriteriaFulfilled(user.getPassword())).thenReturn(true);
+        when(userAccountCreationHelper.emailIsValid(user.getEmail())).thenReturn(true);
+        when(userAccountCreationHelper.
+                createAccountActivationEmailMessage(any(String.class), any(UrlResource.class)))
+                .thenReturn(randomMessage2);
+        userService.createUser(this.user);
+        verify(emailService, times(1)).sendEmail(user.getEmail(),
+                UserAccountCreationHelper.accountActivationEmailSubject, randomMessage2);
+        verify(bookingClient, times(1)).createCart(createdUser.getId());
+    }
 }
