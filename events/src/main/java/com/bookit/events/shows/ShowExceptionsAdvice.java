9592e51e83db5b6421da12c362cb50b9b835c89a
Loveleen C <loveleenchaudhari@gmail.com>
2026-04-20 19:21:25 +0530

    showsController: fix exception handling for responses

--- events/src/main/java/com/bookit/events/shows/ShowExceptionsAdvice.java
+++ events/src/main/java/com/bookit/events/shows/ShowExceptionsAdvice.java
@@ -2,44 +2,44 @@ package com.bookit.events.shows;
 
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@RestControllerAdvice(assignableTypes = {ShowsController.class})
 public class ShowExceptionsAdvice {
     @ExceptionHandler(NullPointerException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    String nullDataInRequestHandler(NullPointerException e){
-        return "Data appears to be missing in the request. Please check the request details and try again";
+    ResponseEntity<String> nullDataInRequestHandler(NullPointerException e){
+        return new ResponseEntity<>("Data appears to be missing in the request. Please check the request details and try again",
+                HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(UnsupportedOperationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    String invalidTicketStatusHandler(UnsupportedOperationException e){
-        return "The ticket status cannot be changed to the requested status.";
+    ResponseEntity<String>  invalidTicketStatusHandler(UnsupportedOperationException e){
+        return new ResponseEntity<>("The ticket status cannot be changed to the requested status.",HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(ResourceNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    String resourceNotFoundHandler(ResourceNotFoundException e){
-        return "Resource not found";
+    void resourceNotFoundHandler(ResourceNotFoundException e){
     }
 
     @ExceptionHandler(DataAccessException.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    String dataAccessExceptionHandler(DataAccessException e){
-        return "Unable to fetch details at the moment. Please try later";
+    ResponseEntity<String> dataAccessExceptionHandler(DataAccessException e){
+        String reason = "Unable to fetch details at the moment. Please try later";
+        return new ResponseEntity<>(reason, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    String illegalArgumentExceptionHandler(IllegalArgumentException e){
-        return "Incorrect data provided in the request. Please check the request details and try again";
+    ResponseEntity<String> illegalArgumentExceptionHandler(IllegalArgumentException e){
+        return new ResponseEntity<>("Incorrect data provided in the request. Please check the request details and try again",HttpStatus.BAD_REQUEST);
     }
 
     //TODO: add resource creation exception
     @ExceptionHandler(ResourceCreationException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    String resourceCreationExceptionHandler(ResourceCreationException e){
-        return "Incorrect data provided in the request. Please check the request details and try again";
+    ResponseEntity<String> resourceCreationExceptionHandler(ResourceCreationException e){
+        return new ResponseEntity<>("Incorrect data provided in the request. Please check the request details and try again",HttpStatus.BAD_REQUEST);
     }
 }
